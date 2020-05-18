acd8be36d722a03e47ec8c49dc2f57bba500cfd0
javiercara <javier.cara@upm.es>
2020-05-18 12:24:47 +0200

    v0.2.0

--- src/EmACQR.jl
+++ src/EmACQR.jl
@@ -9,6 +9,7 @@ export
 	ACQR_kfilter, ACQR_kfilter_s,
 	ACQR_ksmoother, ACQR_ksmoother_s,
 	ACQR_em, ACQR_em_s,
+	ACQR_em_starting,
 	ACQRS_sub,
 	simula01, ACQR_simula,
 	ACQR_em_run, ACQR_em_s_run
